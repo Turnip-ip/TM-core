1c19b489f003d1dbe65639f0074a8123d4509478
HugoMartel <HugoMartel@users.noreply.github.com>
2024-10-29 21:31:40 +0000

    Support for multiple grammars (#3)
    * Changed lib organization for multiple grammars, no newline needed anymore & lint fix
    
    * We now build an generic AST from the Pairs of pest that are specific to each grammar version. And changed minor files locations for better organization
    
    * V0 is now working with the new generic AST
    
    * New tests (#8)
    
    * add tests
    
    * Update tm_v0.pest - comments
    * Update tm_v0.pest - head_move
    
    * Update lib.rs
    
    * Fixed functions names and args only showing tokens
    * Typo -0 in add1 test & removed snake_case warning on tests
    
    ---------
    
    Co-authored-by: benjamin Court <benjamin.court@ens-lyon.fr>
    
    * Working grammar v1, all tests passing
    + minor fixes
    removed test from make debug dependencies
    fixed typoes in few tests
    
    ---------
    
    Co-authored-by: benjamin Court <benjamin.court@ens-lyon.fr> efea66793228dad4b1b959f44886fd38e8f11b23

--- tm_parser.d.ts
+++ tm_parser.d.ts
@@ -1,17 +1,56 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
+ * Takes a TM machine (.tm) code and turns it into a .dot graph code.
+ *
+ * # Examples
+ *
+ * In Rust, it would be used like this:
+ * ```rust
+ * println!("{}", parsing::tm_string_to_dot(&str, "Function Name", 1))
+ * ```
+ *
+ * However, using it in an HTML context might be more understandable since this is
+ * its main purpose.
+ * ```html
+ * <html>
+ *   <head></head>
+ *   <body>
+ *     <textarea id="test-tm-code" name="test-tm-code" rows="20" cols="50">
+ *       Turing Machine code goes here
+ *     </textarea>
+ *     <p id="test-dot-output">
+ *       output
+ *     </p>
+ *     <script type="module">
+ *       import init, { tm_string_to_dot } from "./tm_parser/tm_parser.js";
+ *
+ *       init().then(() => {
+ *         const code_editor = document.getElementById("test-tm-code");
+ *         const dot_output = document.getElementById("test-dot-output");
+ *
+ *         code_editor.addEventListener("input", (e) => {
+ *           console.log("RUST CALL");
+ *           dot_output.innerText = tm_string_to_dot(code_editor.value, "TEST");
+ *           console.log("PARSED");
+ *         });
+ *       });
+ *     </script>
+ *   </body>
+ * </html>
+ * ```
  * @param {string} input_string
  * @param {string} tm_name
+ * @param {number} grammar_version
  * @returns {string}
  */
-export function tm_string_to_dot(input_string: string, tm_name: string): string;
+export function tm_string_to_dot(input_string: string, tm_name: string, grammar_version: number): string;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly tm_string_to_dot: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly tm_string_to_dot: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
--- tm_parser.js
+++ tm_parser.js
@@ -83,11 +83,50 @@ function getStringFromWasm0(ptr, len) {
     return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
 }
 /**
+ * Takes a TM machine (.tm) code and turns it into a .dot graph code.
+ *
+ * # Examples
+ *
+ * In Rust, it would be used like this:
+ * ```rust
+ * println!("{}", parsing::tm_string_to_dot(&str, "Function Name", 1))
+ * ```
+ *
+ * However, using it in an HTML context might be more understandable since this is
+ * its main purpose.
+ * ```html
+ * <html>
+ *   <head></head>
+ *   <body>
+ *     <textarea id="test-tm-code" name="test-tm-code" rows="20" cols="50">
+ *       Turing Machine code goes here
+ *     </textarea>
+ *     <p id="test-dot-output">
+ *       output
+ *     </p>
+ *     <script type="module">
+ *       import init, { tm_string_to_dot } from "./tm_parser/tm_parser.js";
+ *
+ *       init().then(() => {
+ *         const code_editor = document.getElementById("test-tm-code");
+ *         const dot_output = document.getElementById("test-dot-output");
+ *
+ *         code_editor.addEventListener("input", (e) => {
+ *           console.log("RUST CALL");
+ *           dot_output.innerText = tm_string_to_dot(code_editor.value, "TEST");
+ *           console.log("PARSED");
+ *         });
+ *       });
+ *     </script>
+ *   </body>
+ * </html>
+ * ```
  * @param {string} input_string
  * @param {string} tm_name
+ * @param {number} grammar_version
  * @returns {string}
  */
-export function tm_string_to_dot(input_string, tm_name) {
+export function tm_string_to_dot(input_string, tm_name, grammar_version) {
     let deferred3_0;
     let deferred3_1;
     try {
@@ -96,7 +135,7 @@ export function tm_string_to_dot(input_string, tm_name) {
         const len0 = WASM_VECTOR_LEN;
         const ptr1 = passStringToWasm0(tm_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         const len1 = WASM_VECTOR_LEN;
-        wasm.tm_string_to_dot(retptr, ptr0, len0, ptr1, len1);
+        wasm.tm_string_to_dot(retptr, ptr0, len0, ptr1, len1, grammar_version);
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         deferred3_0 = r0;
