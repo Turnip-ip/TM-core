fae6262ed0c3053694b844e0ee2a23a513d24606
benjamin Court <benjamin.court@ens-lyon.fr>
2024-10-30 13:36:09 +0100

    Update lib.rs - add 9 tests v1 (v2)

--- src/lib.rs
+++ src/lib.rs
@@ -140,4 +140,112 @@ mod tests {
             fs::read_to_string("tests/v1/add.tm.result").expect("cannot read file..")
         );
     }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_minus_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/minus.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "MINUS", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "MINUS", 1),
+            fs::read_to_string("tests/v1/minus.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_leq_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/leq.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "LEQ", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "LEQ", 1),
+            fs::read_to_string("tests/v1/leq.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_geq_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/geq.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "GEQ", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "GEQ", 1),
+            fs::read_to_string("tests/v1/geq.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_mul_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/mul.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "MUL", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "MUL", 1),
+            fs::read_to_string("tests/v1/mul.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_exp_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/exp.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "EXP", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "EXP", 1),
+            fs::read_to_string("tests/v1/exp.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_div_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/div.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "DIV", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "DIV", 1),
+            fs::read_to_string("tests/v1/div.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_mod_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/mod.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "MOD", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "MOD", 1),
+            fs::read_to_string("tests/v1/mod.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_is_prime_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/is_prime.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "IS_PRIME", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "IS_PRIME", 1),
+            fs::read_to_string("tests/v1/is_prime.tm.result").expect("cannot read file..")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn v1_lenght_syracuse_parsing() {
+        let input_string: String =
+            fs::read_to_string("tests/v1/lenght_syracuse.tm").expect("cannot read file..");
+
+        dbg!(parser::tm_string_to_dot(&input_string, "LENGHT_SYRACUSE", 1));
+        assert_eq!(
+            parser::tm_string_to_dot(&input_string, "LENGHT_SYRACUSE", 1),
+            fs::read_to_string("tests/v1/lenght_syracuse.tm.result").expect("cannot read file..")
+        );
+    }
 }
