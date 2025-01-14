f6b74b43c6fb5537a8943bdc0569c105d359f7d0
Elouan Oger <elouan.oger.44@gmail.com>
2025-01-14 15:11:05 +0100

    Added a second tape to the core

--- src/turing_machines.rs
+++ src/turing_machines.rs
@@ -18,20 +18,30 @@ pub mod machines {
 
     #[derive(Debug, Clone, Copy)]
     pub struct BaseAction {
-        letter: Gamma,
-        mov: Movement,
+        letter1: Gamma,
+        mov1: Movement,
+        letter2: Gamma,
+        mov2: Movement,
     }
 
     #[derive(Debug, Clone, Copy)]
     pub enum Fun {
-        Mv(i32),
+        Mv1(i32),
+        Mv2(i32),
     }
 
     impl Fun {
-        fn eval(&self, pos: &mut TapePos, _tape: &mut Vec<Gamma>) {
+        fn eval(
+            &self,
+            pos1: &mut TapePos,
+            _tape1: &mut Vec<Gamma>,
+            pos2: &mut TapePos,
+            _tape2: &mut Vec<Gamma>,
+        ) {
             match &self {
                 // TODO check bounds
-                Fun::Mv(i) => *pos = (*pos).wrapping_add(*i as u32),
+                Fun::Mv1(i) => *pos1 = (*pos1).wrapping_add(*i as u32),
+                Fun::Mv2(i) => *pos2 = (*pos2).wrapping_add(*i as u32),
             }
         }
     }
@@ -53,19 +63,21 @@ pub mod machines {
     pub struct TM {
         _state_of_string: HashMap<String, State>,
         _string_of_state: HashMap<State, String>,
-        delta: Vec<Vec<Outcome>>, // delta[state][letter]
+        delta: Vec<Vec<Vec<Outcome>>>, // delta[state][letter1][letter2]
     }
 
     #[wasm_bindgen]
     pub struct Simu {
         _tm: TM,
         _cur_state: State,
-        _head_pos: TapePos,
-        _tape: Vec<Gamma>,
+        _head_pos1: TapePos,
+        _tape1: Vec<Gamma>,
+        _head_pos2: TapePos,
+        _tape2: Vec<Gamma>,
     }
 
     impl TM {
-        pub fn from_state_vector(&self, v: Vec<parser::State>) -> TM {
+        pub fn from_state_vector(&self, _v: Vec<parser::State>) -> TM {
             todo!()
         }
     }
@@ -84,22 +96,35 @@ pub mod machines {
     impl Simu {
         fn step(&mut self) {
             let cur_state_usize = self._cur_state as usize;
-            let head_pos_usize = self._head_pos as usize;
-            let tape_letter = self._tape[head_pos_usize] as usize;
+            let head_pos1_usize = self._head_pos1 as usize;
+            let head_pos2_usize = self._head_pos2 as usize;
+            let tape_letter1 = self._tape1[head_pos1_usize] as usize;
+            let tape_letter2 = self._tape2[head_pos2_usize] as usize;
             let tm = &self._tm;
-            let oc = &tm.delta[cur_state_usize][tape_letter];
+            let oc = &tm.delta[cur_state_usize][tape_letter1][tape_letter2];
             match &(oc.action) {
                 Action::BaseAction(act) => {
-                    self._tape[head_pos_usize] = act.letter;
-                    self._head_pos = match act.mov {
+                    self._tape1[head_pos1_usize] = act.letter1;
+                    self._tape2[head_pos2_usize] = act.letter2;
+                    self._head_pos1 = match act.mov1 {
                         // TODO check bounds
-                        Movement::Left => self._head_pos - 1,
-                        Movement::Right => self._head_pos + 1,
+                        Movement::Left => self._head_pos1 - 1,
+                        Movement::Right => self._head_pos1 + 1,
+                    };
+                    self._head_pos2 = match act.mov2 {
+                        // TODO check bounds
+                        Movement::Left => self._head_pos2 - 1,
+                        Movement::Right => self._head_pos2 + 1,
                     };
                 }
                 Action::Funs(fs) => {
                     for f in fs.iter() {
-                        f.eval(&mut self._head_pos, &mut self._tape);
+                        f.eval(
+                            &mut self._head_pos1,
+                            &mut self._tape1,
+                            &mut self._head_pos2,
+                            &mut self._tape2,
+                        );
                     }
                 }
             }
@@ -113,8 +138,12 @@ pub mod machines {
             self._cur_state
         }
 
-        pub fn head_pos(&self) -> TapePos {
-            self._head_pos
+        pub fn head_pos1(&self) -> TapePos {
+            self._head_pos1
+        }
+
+        pub fn head_pos2(&self) -> TapePos {
+            self._head_pos2
         }
     }
 }
