1707a3edd591c1b8400f6d87ff621f7c1d28b618
Elouan Oger <elouan.oger.44@gmail.com>
2025-01-14 14:07:07 +0100

    refactored types

--- src/turing_machines.rs
+++ src/turing_machines.rs
@@ -1,5 +1,6 @@
 //! TODO documentation
 #![warn(missing_docs)]
+#![allow(dead_code)]
 
 pub mod machines {
     use crate::dot_parsing::parser;
@@ -16,8 +17,7 @@ pub mod machines {
     pub type Gamma = u8;
 
     #[derive(Debug, Clone, Copy)]
-    pub struct ClassOutcome {
-        state: State,
+    pub struct BaseAction {
         letter: Gamma,
         mov: Movement,
     }
@@ -28,17 +28,24 @@ pub mod machines {
     }
 
     impl Fun {
-        fn eval(&self, state: &mut State, pos: &mut TapePos, tape: &mut Vec<Gamma>) {
+        fn eval(&self, pos: &mut TapePos, _tape: &mut Vec<Gamma>) {
             match &self {
-                Fun::Mv(i) => *pos = *pos + (*i as u32),
+                // TODO check bounds
+                Fun::Mv(i) => *pos = (*pos).wrapping_add(*i as u32),
             }
         }
     }
 
     #[derive(Debug, Clone)]
-    pub enum Outcome {
-        Classical(ClassOutcome),
+    pub enum Action {
+        BaseAction(BaseAction),
         Funs(Vec<Fun>),
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Outcome {
+        action: Action,
+        target: State,
     }
 
     #[wasm_bindgen]
@@ -46,14 +53,19 @@ pub mod machines {
     pub struct TM {
         _state_of_string: HashMap<String, State>,
         _string_of_state: HashMap<State, String>,
-        _cur_state: State,
-        _head_pos: TapePos,
-        _tape: Vec<Gamma>,
         delta: Vec<Vec<Outcome>>, // delta[state][letter]
     }
 
+    #[wasm_bindgen]
+    pub struct Simu {
+        _tm: TM,
+        _cur_state: State,
+        _head_pos: TapePos,
+        _tape: Vec<Gamma>,
+    }
+
     impl TM {
-        pub fn of_state_vector(&self, v: Vec<parser::State>) -> TM {
+        pub fn from_state_vector(&self, v: Vec<parser::State>) -> TM {
             todo!()
         }
     }
@@ -67,38 +79,42 @@ pub mod machines {
         pub fn string_of_state(&self, s: State) -> String {
             self._string_of_state.get(&s).unwrap().clone()
         }
+    }
 
+    impl Simu {
+        fn step(&mut self) {
+            let cur_state_usize = self._cur_state as usize;
+            let head_pos_usize = self._head_pos as usize;
+            let tape_letter = self._tape[head_pos_usize] as usize;
+            let tm = &self._tm;
+            let oc = &tm.delta[cur_state_usize][tape_letter];
+            match &(oc.action) {
+                Action::BaseAction(act) => {
+                    self._tape[head_pos_usize] = act.letter;
+                    self._head_pos = match act.mov {
+                        // TODO check bounds
+                        Movement::Left => self._head_pos - 1,
+                        Movement::Right => self._head_pos + 1,
+                    };
+                }
+                Action::Funs(fs) => {
+                    for f in fs.iter() {
+                        f.eval(&mut self._head_pos, &mut self._tape);
+                    }
+                }
+            }
+            self._cur_state = oc.target;
+        }
+    }
+
+    #[wasm_bindgen]
+    impl Simu {
         pub fn cur_state(&self) -> State {
             self._cur_state
         }
 
         pub fn head_pos(&self) -> TapePos {
             self._head_pos
-        }
-
-        pub fn tape(&self) -> *const Gamma {
-            self._tape.as_ptr()
-        }
-
-        pub fn step(&mut self) {
-            let cur_state = self._cur_state as usize;
-            let tape_letter = self._tape[self._head_pos as usize] as usize;
-            match &(self.delta[cur_state][tape_letter]) {
-                Outcome::Classical(oc) => {
-                    self._tape[self._head_pos as usize] = oc.letter;
-                    self._cur_state = oc.state;
-                    self._head_pos = match oc.mov {
-                        // TODO check bounds
-                        Movement::Left => self._head_pos - 1,
-                        Movement::Right => self._head_pos + 1,
-                    };
-                }
-                Outcome::Funs(fs) => {
-                    for f in fs.iter() {
-                        f.eval(&mut self._cur_state, &mut self._head_pos, &mut self._tape);
-                    }
-                }
-            }
         }
     }
 }
